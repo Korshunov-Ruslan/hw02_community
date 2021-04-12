6058d288eb03d12d2aec4c4d5a698a75ef7701ec
Korshunov-Ruslan <thenyky1937@gmail.com>
2021-04-12 15:58:49 +0300

    PEP8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=80)
     slug = models.SlugField(max_length=80, unique=True)
     description = models.TextField()
 
@@ -18,4 +18,5 @@ class Post(models.Model):
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='posts')
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True,
+                              null=True)
