9151ef290929d514c7ceccb5e8f1a7126416298c
Korshunov-Ruslan <thenyky1937@gmail.com>
2021-04-15 16:43:54 +0300

    some fixes

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -5,7 +5,7 @@ from .models import Group, Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-    search_fields = ('tex',)
+    search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
