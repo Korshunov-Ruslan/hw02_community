15f2a104b9ef5168807544dbe77707059f4b86f4
Korshunov-Ruslan <thenyky1937@gmail.com>
2021-09-08 19:13:14 +0300

    small fixes and added -about- app for static pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,11 +6,15 @@ from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'index.html', context)
 
 
 def group_posts(request, slug):
